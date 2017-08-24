4baf027d2d3f31012c5631500329cd865db3fda8
Tanmay <shahtany99@gmail.com>
2017-08-24 15:09:41 -0400

    init(module-client): Vehicle-List Module Initialization

--- client/cartracker/src/app/app.module.ts
+++ client/cartracker/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VehicleListComponent
   ],
   imports: [
     BrowserModule
